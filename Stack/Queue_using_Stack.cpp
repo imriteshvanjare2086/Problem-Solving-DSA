0f5a25c1af2236b71ab9d123fd0a5565f977d5d8
imriteshvanjare2086 <iriteshvanjare20@gmail.com>
2026-01-27 21:32:05 +0530

    Add solution of Implementation of Queue using Stack

---
+++ Stack/Queue_using_Stack.cpp
@@ -0,0 +1,47 @@
+// Problem: Implement Queue using Stack
+// Platform: LeetCode
+// Time Complexity: Push O(n), Pop O(1), Top O(1)
+// Space Complexity: O(n)
+
+#include<stack>
+using namespace std;
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    
+    stack <int> s1;
+    stack <int> s2;
+
+    void push(int x) {
+
+        while(!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }   
+        s1.push(x);
+
+        while(!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        } 
+    }
+    
+    int pop() {
+        int ans = s1.top();
+        s1.pop();
+        return ans;
+    }
+    
+    int peek() {
+        return s1.top();
+    }
+    
+    bool empty() {
+        return s1.empty();
+    }
+
+};
