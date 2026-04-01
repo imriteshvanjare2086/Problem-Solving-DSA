add8a380b707f1e125d0dac166fa589f879b6886
Sahil Vanjare <sahilvanjare78@gmail.com>
2026-04-02 00:58:47 +0530

    Added solution of linked list cycle

---
+++ Linked-List/Linked List Cycle.cpp
@@ -0,0 +1,35 @@
+// Problem: Merge Two Sorted Lists
+// Platform: LeetCode
+// Time Complexity: O(n)
+// Space Complexity: O(n)  
+
+
+//  Definition for singly-linked list.
+struct ListNode {
+    int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(nullptr) {}
+};
+ 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) 
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
