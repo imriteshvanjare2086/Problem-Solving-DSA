acf487a8b0bae16e8f1c8170ffa3482de962ec0d
Ritesh Vanjare <iriteshvanjare20@gmail.com>
2026-05-03 23:56:28 +0530

    change

--- Arrays/Single Number.cpp
+++ Arrays/Single Number.cpp
@@ -11,7 +11,8 @@ class Solution
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-        for (int i : nums) {
+        for (int i : nums) 
+        {
             ans ^= i;
         }
         return ans;
