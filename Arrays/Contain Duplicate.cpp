b89e57c097b85ee53699cbe9b03b5f3c606b8058
Ritesh Vanjare <iriteshvanjare20@gmail.com>
2026-05-20 23:15:36 +0530

    added solution of Comntain Duplicate

---
+++ Arrays/Contain Duplicate.cpp
@@ -0,0 +1,17 @@
+// Problem: Contain Duplicate
+// Platform: LeetCode
+// Time Complexity: O(logn)
+// Space Complexity: O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution 
+{
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        set <int> s(nums.begin(),nums.end());
+        return s.size() != nums.size();    
+    }
+};
