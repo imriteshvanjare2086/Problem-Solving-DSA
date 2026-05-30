6114655b3317c8054058232fe7131bdfa1784ec3
Ritesh Vanjare <iriteshvanjare20@gmail.com>
2026-05-30 21:44:04 +0530

    Added solution of find k-or in an array

--- Arrays/Find K-or.cpp
+++ Arrays/Find K-or.cpp
@@ -1,7 +1,7 @@
-// Problem: Find the k-or of an array
+// Problem: Find the K-Or of an Array
 // Platform: LeetCode
-// Time Complexity: O(n^2)
-// Space Complexity: O(n)
+// Time Complexity: O(n * B)
+// Space Complexity: O(n * B)
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -11,6 +11,7 @@ public:
     int findKOr(vector<int>& nums, int k) 
     {
         int Bits=0;
+
         for(int x : nums)
         {
             if(x!=0)
@@ -18,52 +19,65 @@ public:
                 Bits = max(Bits, 32 - __builtin_clz(x));
             }
         }
+
+        if(Bits==0)
+        {
+            return 0;
+        }
+
         vector <string> v;
+
         for(int i : nums)
         {
             string s;
+
             while(i>0)
             {
                 s+=(i%2)+'0';
                 i/=2;
             }
+
             while(s.size() < Bits)
             {
                 s+='0';
             }
+
             reverse(s.begin(),s.end());
-            cout << s << endl;
             v.push_back(s);
-        }   
+        }
+
         string ans;
+
         for(int i=0;i<v[0].size();i++)
         {
-            int ob=0; 
-            int zb=0; 
+            int ob=0;
+
             for(int j=0;j<v.size();j++)
             {
                 if(v[j][i] == '1')
                 {
-                    ob++;       
-                }
-                else
-                {
-                    zb++;
+                    ob++;
                 }
             }
-            ans+=(ob>=k)? '1' :'0';
+
+            ans+=(ob>=k)? '1' : '0';
         }
+
         reverse(ans.begin(),ans.end());
+
         long long base=1;
         int val=0;
+
         for(char c : ans)
         {
             if(c=='1')
             {
                 val+=base;
             }
+
             base*=2;
         }
+
         return val;
     }
 };
