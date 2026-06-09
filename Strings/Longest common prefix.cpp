d3973fa4d3cc2282748b1bed546d2b4c64552061
Ritesh Vanjare <iriteshvanjare20@gmail.com>
2026-06-09 17:37:50 +0530

    Added solution of longest common prefix

---
+++ Strings/Longest common prefix.cpp
@@ -0,0 +1,28 @@
+// Problem: Longest common prefix
+// Platform: LeetCode
+// Time Complexity: O(n*m) 
+// Space Complexity: O(m) 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) 
+    {
+        string result;
+        for(int i=0;i<strs[0].size();i++)
+        {
+            char ch = strs[0][i];
+            for(int j=0;j<strs.size();j++)
+            {
+                if(strs[j][i] != ch)
+                {
+                   return result;
+                }
+            }
+            result+=ch;
+        }    
+        return result;
+    }
+};
