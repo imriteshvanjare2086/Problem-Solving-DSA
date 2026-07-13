b90cb401c648307dfe13ecb129978933f3ecd597
Ritesh Vanjare <iriteshvanjare20@gmail.com>
2026-07-13 23:35:06 +0530

    Added solution of rem ll elem

---
+++ Linked-List/remove ll ele.cpp
@@ -0,0 +1,38 @@
+// Problem: Remove Linked List Elements
+// Platform: LeetCode
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Definition for singly-linked list.
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* temp = dummy;
+
+        while (temp->next != NULL) {
+            if (temp->next->val == val) {
+                temp->next = temp->next->next;
+            } else {
+                temp = temp->next;
+            }
+        }
+
+        return dummy->next;
+    }
+};
