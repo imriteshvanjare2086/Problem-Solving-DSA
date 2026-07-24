9c994e3d2943838d8bff30ea15f4ca5ef221acc8
Ritesh Vanjare <iriteshvanjare20@gmail.com>
2026-07-24 23:45:46 +0530

    Added solution of power function

---
+++ Maths/pow.cpp
@@ -0,0 +1,16 @@
+// Problem: power
+// Platform: LeetCode
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) 
+    {
+        double power = pow(x,n);
+        return power;
+    }
+};
