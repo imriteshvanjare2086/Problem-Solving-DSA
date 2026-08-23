7996d2f87598e5c7f6fd1e36017689581435cafd
Ritesh Vanjare <iriteshvanjare20@gmail.com>
2026-08-23 23:46:40 +0530

    added solution for sort array by parity

---
+++ Arrays/sort by parity.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector <int> v;
+        for(int i : nums)
+        {
+            if(i%2 == 0)
+            {
+                v.push_back(i);
+            }
+        }
+        for(int i : nums)
+        {
+            if(i%2 != 0)
+            {
+                v.push_back(i);
+            }
+        }
+        return v;
+    }
+};
